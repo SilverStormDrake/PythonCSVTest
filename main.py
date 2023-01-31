4e49f37893be225ac0e521226a84a6cf8ee6909b
André Luiz <andrelu0022@gmail.com>
2023-01-31 03:52:14 -0300

    Trying some API stuff

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import csv
+import requests
+import json
+
+
+
+param ={
+    "Gilded Drake"
+}
 
 def main():
+    
     with open('cards.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in reader:
-            print(', '.join(row))
-
+    #        print(', '.join(row))
+    
+    #response = requests.get()
+    print(response.status_code)
 
 
 if __name__ == "__main__":
